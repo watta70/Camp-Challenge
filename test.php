2ae77db4eccf116cfdf08f7a54b700929daad57a
maoya-watabe <watta700@gmail.com>
2016-06-06 17:19:09 +0900

    PHPプログラミング基礎学習　課題 基礎1~6 + 応用7

--- test.php
+++ test.php
@@ -27,5 +27,35 @@ else{if($piko == 'a'){echo '文字です！';
 else{echo 'その他です！';
 }}}
 
+echo"<br/><br/>";
 
+
+//7
+
+//➀
+echo "雑貨<br>";
+echo "生鮮食品<br>";
+echo "その他<br><br>";
+
+
+//➁
+$param1 = $_GET['param1'];//個数
+$param2 = $_GET['param2'];//総額
+$param3 = $_GET['param3'];//商品種別
+echo "$param3";
+echo "<br>一個";
+echo $param2 / $param1;
+echo '円';
+echo "<br>合計";
+echo "$param2";
+echo '円';
+echo "<br><br>";
+
+
+//➂
+
+if($param2>=3000){echo $param2 * 0.04;
+}else{if ($param2>=5000){echo $param2 * 0.05;
+}else{echo "0";
+}}echo 'ポイント';
  ?>
