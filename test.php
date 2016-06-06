24f0b71c041c949eb1dee57ea450ac8bad193ae1
maoya-watabe <watta700@gmail.com>
2016-06-06 15:25:22 +0900

    PHPプログラミング基礎学習１　課題：基礎編

---
+++ test.php
@@ -0,0 +1,31 @@
+<?php
+//課題基礎編
+//1
+echo "hello world<br/>";
+
+//2
+echo "groove-gear<br><br>";
+
+//3
+echo "初めまして、渡部です。<br>";
+echo "今はプログラミングの勉強をしています。<br>";
+echo "趣味は読書、スノボー、フットサル、料理などです。<br><br>";
+
+//4,5
+const X = "5";
+$hensu = "10";
+echo ++$hensu + 4 / X;
+echo"<br/><br/>";
+
+//6
+$piko = "a";
+if ($piko == 1){echo '1です！';
+}else{if($piko == 2){echo 'プログラミングキャンプ！';
+}
+else{if($piko == 'a'){echo '文字です！';
+}
+else{echo 'その他です！';
+}}}
+
+
+ ?>
