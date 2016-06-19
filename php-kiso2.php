63ec4cdfbe9ce50f7ddb9bc7a21da1ec0c7b9f9b
maoya-watabe <watta700@gmail.com>
2016-06-19 15:02:03 +0900

    no message

---
+++ php-kiso2.php
@@ -0,0 +1,105 @@
+<?php
+//課題１
+/*$num = 1;
+    $message = '  ';
+    switch($num) {
+        case 1:
+            $message = 'one';
+            break;
+        case 2:
+            $message = 'two';
+            break;
+        default:
+            $message = '想定外';
+            break;
+    }
+    echo $message;
+*/
+//課題２
+/*$num = 'q';
+    $message = '  ';
+    switch($num) {
+        case 'A':
+            $message = '英語';
+            break;
+        case 'あ':
+            $message = '日本語';
+            break;
+        default:
+            break;
+    }
+    echo $message;
+*/
+//課題３　for文を利用して、8を20回掛ける処理を実現してください。
+/*
+$numm = 8;
+for($i=0; $i<20; $i++) {
+        echo $numm = $numm * 8 ;
+        echo "<br/>" ;
+    }
+*/
+//課題４
+/*for($i=0; $i<30; $i++) {
+        echo 'A';
+    }
+*/
+//課題５　for文を利用して、0から100を全部足す処理を実現してください。
+/*$tasu = 0;
+for($i=0; $i<102; $i++) {
+        echo $tasu = $tasu + ($tasu + 1);
+        echo "<br/>";
+    }
+*/
+/*課題６．while文を利用して、以下の処理を実現してください。
+　　　　1000を2で割り続け、100より小さくなったらループを抜ける処理*/
+/*$key = 1000;
+   while ($key >= 100) {
+       echo $key = $key / 2 ;
+       echo "<br/>";
+      }
+*/
+/*課題７ 以下の順番で、要素が格納された配列を作成してください。
+　　　　10, 100, 'soeda', 'hayashi', -20, 118, 'END' */
+/*    $arr = array(10,100,'soeda','hayashi',-20,118,'END');
+
+*/
+// 課題８　７で作成した配列の'soeda'を33に変更してください。
+/* $arr = array(10,100,'soeda','hayashi',-20,118,'END');
+    $arr[2] = 33;
+    echo $arr[2];
+    */
+/*課題９　以下の順で、連想配列を作成してください。
+　　　　1に'AAA', 'hello'に'world', 'soeda'に33, 20に20 */
+/*$arr = array('1' => 'AAA',
+             'hello' => 'world',
+             'soeda' => 33,
+             20 => 20
+              );
+        var_dump($arr);
+*/
+/*課題１０ 保留 クエリストリングを利用して、以下の処理を実現してください。
+
+　簡易素因数分解のロジックを作成します。
+
+　クエリストリングから渡された数値を1ケタの素数で可能な限り分解し、
+　元の値と素因数分解の結果を表示するようにしてください。
+　
+　2ケタ以上の素数が含まれた数値の場合は、
+　「元の値　1ケタの素因数　その他」と表記して、その他　に含んでください */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
